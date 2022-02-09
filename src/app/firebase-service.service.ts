17640220c799e65492f979ff7d299fe6f64b5da3
Vianney-Gln <vgeloen@gmail.com>
2022-02-09 14:17:27 +0100

    created getGameById function in service

--- src/app/firebase-service.service.ts
+++ src/app/firebase-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { initializeApp } from "firebase/app";
-import {collection, DocumentData, getDocs, getFirestore} from "firebase/firestore/lite";
+import {collection, doc, DocumentData, getDoc, getDocs, getFirestore} from "firebase/firestore/lite";
 
 const TABLEGAME:string = "list-video-games";
 
@@ -8,17 +8,19 @@ const TABLEGAME:string = "list-video-games";
   providedIn: 'root'
 })
 export class FirebaseServiceService {
+//constructor
+  constructor() { }
 
   //config Firebase
-  private _firebaseConfig = {
-    apiKey: "AIzaSyAr6kWMth8eAuV-arP_iu0Sxs933CnjdcM",
-    authDomain: "list-video-games.firebaseapp.com",
-    databaseURL: "https://list-video-games-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "list-video-games",
-    storageBucket: "list-video-games.appspot.com",
-    messagingSenderId: "876715667140",
-    appId: "1:876715667140:web:ca73580e184ed5ce21e391"
-  };
+	private _firebaseConfig = {
+    	apiKey: "AIzaSyAr6kWMth8eAuV-arP_iu0Sxs933CnjdcM",
+    	authDomain: "list-video-games.firebaseapp.com",
+    	databaseURL: "https://list-video-games-default-rtdb.europe-west1.firebasedatabase.app",
+    	projectId: "list-video-games",
+    	storageBucket: "list-video-games.appspot.com",
+    	messagingSenderId: "876715667140",
+    	appId: "1:876715667140:web:ca73580e184ed5ce21e391"
+  	};
 
   //init firebase
   app = initializeApp(this._firebaseConfig);
@@ -26,14 +28,33 @@ export class FirebaseServiceService {
 
   //function getting all games from db
   public async getAllGames():Promise<{data: DocumentData;id: string;}[]>{
-    const GAME_COLLECTION = collection(this.db,TABLEGAME);
-    const GAME_SNAPSHOT = await getDocs(GAME_COLLECTION);
+try {
+  const GAME_COLLECTION = collection(this.db,TABLEGAME);
+  const GAME_SNAPSHOT = await getDocs(GAME_COLLECTION);
     return GAME_SNAPSHOT.docs.map((doc)=>{
-      return {
-        data:doc.data(),
-        id:doc.id
-      }
+        return {
+          data:doc.data(),
+          id:doc.id
+        }
     })
-  }
-  constructor() { }
+}catch {
+  throw new Error;
 }
+  }
+     //function getting one game by his id
+  public async getGameById(id:string) {
+		try {
+        await getDoc(doc(this.db,TABLEGAME,id));
+
+    	}catch {
+        throw new Error;
+    	}
+  	}
+
+
+
+
+  	}
+
+
+
