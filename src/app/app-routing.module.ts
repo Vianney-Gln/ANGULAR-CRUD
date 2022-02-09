c1bfcc5f721da0980d65f94b24d9a41983e8fb1a
Vianney-Gln <vgeloen@gmail.com>
2022-02-09 12:07:42 +0100

    created a addGame component and routing as addGame in appRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddGameComponent } from './add-game/add-game.component';
 import { ListGameComponent } from './list-game/list-game.component';
 
-const routes: Routes = [{path: '', component: ListGameComponent}];
+const routes: Routes = [
+  {path: '', component: ListGameComponent},
+  {path: 'addGame', component: AddGameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListGameComponent } from './list-game/list-game.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { AddGameComponent } from './add-game/add-game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListGameComponent,
-    TopBarComponent
+    TopBarComponent,
+    AddGameComponent
   ],
   imports: [
     BrowserModule,
