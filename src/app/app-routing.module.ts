3ae2d03ea214f57fba29d1b91174e363e7dc0f52
Vianney-Gln <vgeloen@gmail.com>
2022-02-09 12:02:14 +0100

    created a topBar component and routing listGame as accueil in appRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListGameComponent } from './list-game/list-game.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: ListGameComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListGameComponent } from './list-game/list-game.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListGameComponent
+    ListGameComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
